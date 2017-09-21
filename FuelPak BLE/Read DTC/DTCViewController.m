b883353d7361f52b682bbad9559946fdb1e4e778
msaradeth2 <msaradeth@vanceandhines.com>
2017-09-21 16:25:33 -0700

    Added Background images

--- FuelPak BLE/Read DTC/DTCViewController.m
+++ FuelPak BLE/Read DTC/DTCViewController.m
@@ -54,46 +54,53 @@
     
     NSString *resourceDirectory = [[NSBundle mainBundle] resourcePath];
     NSString *imageName  = [resourceDirectory stringByAppendingPathComponent:@"background33.png"];
-    [self.view setBackgroundColor:[UIColor colorWithPatternImage:  [UIImage imageWithContentsOfFile: imageName]   ]];
-    
+//    [self.view setBackgroundColor:[UIColor colorWithPatternImage:  [UIImage imageWithContentsOfFile: imageName]   ]];
+    [self.uinavbarMain setTintColor: [[EAController sharedController] getNavBarTintColor]];
     
     [self.uinavbarMain setAlpha:  [[EAController sharedController] getNavBarAlpha]];
     [self.uinavbarMain setHidden: [[EAController sharedController] getNavBarHidden]];
     
+    [self.uinavbtnBack setTintColor: [UIColor blackColor] ];
+
     
     
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
-    {
-        [self.uinavbtnBack setTintColor: [UIColor whiteColor] ];
-        [self.uinavbtnRefresh setTintColor: [UIColor whiteColor] ];
-        
-    }
-    else
-    {
-        [self.uinavbtnBack setTintColor: [UIColor clearColor] ];
-        [self.uinavbtnRefresh setTintColor: [UIColor clearColor] ];
-    }
+//    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+//    {
+//        [self.uinavbtnBack setTintColor: [UIColor whiteColor] ];
+//        [self.uinavbtnRefresh setTintColor: [UIColor whiteColor] ];
+//        
+//    }
+//    else
+//    {
+//        [self.uinavbtnBack setTintColor: [UIColor clearColor] ];
+//        [self.uinavbtnRefresh setTintColor: [UIColor clearColor] ];
+//    }
+//    
     
     
-    
-    BOOL navflag = [[EAController sharedController] getNavBarColorOverwritesGradient];
-    if ( navflag==TRUE )
-    {
-        [self.uinavbarMain setTintColor: [[EAController sharedController] getNavBarTintColor]];
-        
-        [self.uinavbarMain setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
-        [self.uinavbarMain setBackgroundImage:nil forBarMetrics:UIBarMetricsLandscapePhone];
-        
-    }
-    else
-    {
-        UIImage *gradientImage44 = [[UIImage imageWithData:[[EAController sharedController] getNavBarBackgroundImageForBarMetricsDefaultName]] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
-        UIImage *gradientImage32 = [[UIImage imageWithData:[[EAController sharedController] getNavBarBackgroundImageForBarMetricsLandscapePhone]] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
-        
-        [self.uinavbarMain setBackgroundImage:gradientImage44 forBarMetrics:UIBarMetricsDefault];
-        [self.uinavbarMain setBackgroundImage:gradientImage32 forBarMetrics:UIBarMetricsLandscapePhone];
-    }
-    
+//    BOOL navflag = [[EAController sharedController] getNavBarColorOverwritesGradient];
+////    navflag = true;
+//    if ( navflag==TRUE )
+//    {
+//        [self.uinavbarMain setTintColor: [[EAController sharedController] getNavBarTintColor]];
+//        [self.uinavbarMain setBackgroundImage:[UIImage imageWithContentsOfFile: imageName] forBarMetrics:UIBarMetricsDefault];
+////        [self.uinavbarMain setBackgroundImage:[UIImage imageWithContentsOfFile: imageName] forBarMetrics:UIBarMetricsCompact];
+//
+//        
+//        [self.uinavbarMain setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+//        [self.uinavbarMain setBackgroundImage:nil forBarMetrics:UIBarMetricsCompact];
+////        [self.uinavbarMain setBackgroundImage:nil forBarMetrics:UIBarMetricsLandscapePhone];
+//        
+//    }
+//    else
+//    {
+//        UIImage *gradientImage44 = [[UIImage imageWithData:[[EAController sharedController] getNavBarBackgroundImageForBarMetricsDefaultName]] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
+//        UIImage *gradientImage32 = [[UIImage imageWithData:[[EAController sharedController] getNavBarBackgroundImageForBarMetricsLandscapePhone]] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
+//        
+//        [self.uinavbarMain setBackgroundImage:gradientImage44 forBarMetrics:UIBarMetricsDefault];
+//        [self.uinavbarMain setBackgroundImage:gradientImage32 forBarMetrics:UIBarMetricsLandscapePhone];
+//    }
+//    
     
     [self.uinavbarMain setTitleTextAttributes:
      [NSDictionary dictionaryWithObjectsAndKeys:
@@ -108,11 +115,21 @@
     
 
 
+    [self setStatusBarColor];
     
 }
 
 
 
+
+
+- (void) setStatusBarColor
+{
+    [[UIApplication sharedApplication] setStatusBarHidden:NO];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    
+
+}
 /*
 #pragma mark - Navigation
 
