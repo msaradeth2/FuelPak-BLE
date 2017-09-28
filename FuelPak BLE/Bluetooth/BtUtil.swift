38fe6e965a4f5dcaac43212a880d91ed4f0f2d47
msaradeth2 <msaradeth@vanceandhines.com>
2017-09-28 11:57:11 -0700

    cleanup folders

---
+++ FuelPak BLE/Bluetooth/BtUtil.swift
@@ -0,0 +1,33 @@
+//
+//  BtUtil.swift
+//  FuelPak BLE
+//
+//  Created by Mike Saradeth on 9/28/17.
+//  Copyright © 2017 Mike Saradeth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//import BlueCapKit
+import CoreBluetooth
+
+
+final class BtUtil: NSObject {
+    
+    // MARK: - Shared Instance
+    
+    static let sharedInstance: BtUtil = {
+        let instance = BtUtil()
+        // setup code
+        return instance
+    }()
+    
+    // MARK: - Initialization Method
+    
+    override init() {
+        super.init()
+    }
+    
+    
+    
+}
